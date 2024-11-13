b50c5f89a521a7279b25cec9da6d14554a42ec42
DHARMIKgohil013 <dharmikgohil395003@gmail.com>
2024-11-13 11:49:25 +0530

    empty

--- Udemy Course/src/TestOpenCV.java
+++
@@ -1,7 +0,0 @@
-import org.opencv.core.Core;
-
-public class TestOpenCV {    public static void main(String[] args) {
-        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-        System.out.println("OpenCV Loaded Successfully");
-    }
-}
