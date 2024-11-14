dadc19ccd12a4958f7cb2c42e9365b0381de8816
DHARMIKgohil013 <dharmikgohil395003@gmail.com>
2024-11-14 18:21:53 +0530

    ass

---
+++ Udemy Course/src/static1.java
@@ -0,0 +1,8 @@
+public class static1 {
+    static final int x=10;
+    public static void main(String[] args) {
+         System.out.println(x);
+         
+         System.out.println(x);
+    }
+}
