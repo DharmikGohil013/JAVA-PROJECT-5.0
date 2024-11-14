b02af2f05f80ff2dc08e9ec3be472997bc7de9b9
DHARMIKgohil013 <dharmikgohil395003@gmail.com>
2024-11-14 17:20:21 +0530

    add

--- Udemy Course/src/anonomyus.java
+++ Udemy Course/src/anonomyus.java
@@ -1,17 +1,22 @@
 public class anonomyus {
     public static void main(String[] args) {
-        
+        outer obj = new outer();
+        obj.meth1();
     }
 }
 
 abstract class my{
-    abstract void display();
+    abstract public void display();
 }
 class outer{
     public void meth1()
     {
         my obj = new my() {
-            System.out.println("sub meth2");
+            public void display()
+            {
+                System.out.println("sub meth2");
+            }
         };
+        obj.display();
     }
 }
