90c05fb581a5866c1368774ec2a028e1015131a4
DHARMIKgohil013 <dharmikgohil395003@gmail.com>
2024-11-21 18:33:51 +0530

    add

--- Udemy Course/src/java1.java
+++ Udemy Course/src/java1.java
@@ -1,7 +1,12 @@
+import java.util.Scanner;
+
 public class java1 
 {
     public static void main(String[] args) 
     {
-            
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine().toUpperCase();
+        input.toLowerCase();
+        System.out.println("output is: "+input.toLowerCase());    
     }
 }
